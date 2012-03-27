a454dff4273c5d9440b0f883d1254c383989d33d
aliquandohb <aliquandohb@gmail.com>
2012-03-27 11:34:34 +0000

    Fix: last bug on TamagoACA2

--- TamagoACA2/ICECCS/test/tamago/aca/bank/TestDepositSecurityNominal.java
+++ TamagoACA2/ICECCS/test/tamago/aca/bank/TestDepositSecurityNominal.java
@@ -23,11 +23,8 @@ public class TestDepositSecurityNominal
         // Step: 0 Method: init
         // Transition: ninit--{init}-->initialized
         try {
-            if(!) {
-                fail("Wrong Test");
-            }
             code.init();
-            boolean __tamagotest_oracle = (code.getOpNumber() < 0);
+            boolean __tamagotest_oracle = ((code.getOpNumber() < 0) && acaInitialised);
             assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
@@ -38,11 +35,12 @@ public class TestDepositSecurityNominal
         try {
             // TODO fulfill the parameter: aca
             tamago.ext.aca2.ACA aca;
-            if(!) {
+            if(!(code.getPlay().isCorrectACA(aca) && "clerk".equals(aca.role) && "banker".equals(aca.role) && !"customer".equals(aca.role) && (("alphonse".equals(aca.user) || "boris".equals(aca.user) || "catherine".equals(aca.user) || "damien".equals(aca.user) || "elise".equals(aca.user) || "franck".equals(aca.user)) && ("customer".equals(aca.role) || "clerk".equals(aca.role) || "banker".equals(aca.role) || "director".equals(aca.role)) && ("montreal".equals(aca.org) || "toronto".equals(aca.org))))) {
                 fail("Wrong Test");
             }
             code.deposit(aca);
-            assertTrue(true);
+            boolean __tamagotest_oracle = ("deposit".equals(code.getHistoric().lastAction()) && (aca == code.getHistoric().lastSecuParam()));
+            assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
             fail(("Test failed at step 1. " + exc.getMessage()));
@@ -52,11 +50,11 @@ public class TestDepositSecurityNominal
         try {
             // TODO fulfill the parameter: aca
             tamago.ext.aca2.ACA aca;
-            if(!) {
+            if(!(code.getPlay().isCorrectACA(aca) && (("alphonse".equals(aca.user) || "boris".equals(aca.user) || "catherine".equals(aca.user) || "damien".equals(aca.user) || "elise".equals(aca.user) || "franck".equals(aca.user)) && ("customer".equals(aca.role) || "clerk".equals(aca.role) || "banker".equals(aca.role) || "director".equals(aca.role)) && ("montreal".equals(aca.org) || "toronto".equals(aca.org))) && code.getHistoric().mustDone("deposit") && (code.getHistoric().getSecuParamFromID("deposit").getUser() == aca.user))) {
                 fail("Wrong Test");
             }
             code.register(aca);
-            boolean __tamagotest_oracle = (code.getOpNumber() > 0);
+            boolean __tamagotest_oracle = ((code.getOpNumber() > 0) && ("register".equals(code.getHistoric().lastAction()) && (aca == code.getHistoric().lastSecuParam())));
             assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
@@ -67,100 +65,40 @@ public class TestDepositSecurityNominal
         try {
             // TODO fulfill the parameter: aca
             tamago.ext.aca2.ACA aca;
-            if(!) {
+            if(!(code.getPlay().isCorrectACA(aca) && (("alphonse".equals(aca.user) || "boris".equals(aca.user) || "catherine".equals(aca.user) || "damien".equals(aca.user) || "elise".equals(aca.user) || "franck".equals(aca.user)) && ("customer".equals(aca.role) || "clerk".equals(aca.role) || "banker".equals(aca.role) || "director".equals(aca.role)) && ("montreal".equals(aca.org) || "toronto".equals(aca.org))))) {
                 fail("Wrong Test");
             }
             code.check(aca);
-            boolean __tamagotest_oracle = (code.getOpNumber() > 0);
+            boolean __tamagotest_oracle = ((code.getOpNumber() > 0) && ("check".equals(code.getHistoric().lastAction()) && (aca == code.getHistoric().lastSecuParam())));
             assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
             fail(("Test failed at step 3. " + exc.getMessage()));
         }
-        // Step: 4 Method: validate_dir
-        // Transition: waited--{validate_dir}-->validateDir
+        // Step: 4 Method: cancel
+        // Transition: waited--{cancel}-->cancelled
         try {
             // TODO fulfill the parameter: aca
             tamago.ext.aca2.ACA aca;
-            if(!) {
+            if(!(code.getPlay().isCorrectACA(aca) && "banker".equals(aca.role) && "director".equals(aca.role) && !"elisa".equals(aca.user) && !"customer".equals(aca.role) && (("alphonse".equals(aca.user) || "boris".equals(aca.user) || "catherine".equals(aca.user) || "damien".equals(aca.user) || "elise".equals(aca.user) || "franck".equals(aca.user)) && ("customer".equals(aca.role) || "clerk".equals(aca.role) || "banker".equals(aca.role) || "director".equals(aca.role)) && ("montreal".equals(aca.org) || "toronto".equals(aca.org))) && code.getHistoric().mustDone("deposit") && (code.getHistoric().getSecuParamFromID("deposit").getUser() == aca.user))) {
                 fail("Wrong Test");
             }
-            code.validate_director(aca);
-            assertTrue(true);
+            code.cancel(aca);
+            boolean __tamagotest_oracle = ("cancel".equals(code.getHistoric().lastAction()) && (aca == code.getHistoric().lastSecuParam()));
+            assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
             fail(("Test failed at step 4. " + exc.getMessage()));
         }
-        // Step: 5 Method: validate
-        // Transition: validateDir--{validate}-->completed
+        // Step: 5 Method: init
+        // Transition: cancelled--{init}-->initialized
         try {
-            // TODO fulfill the parameter: aca
-            tamago.ext.aca2.ACA aca;
-            if(!) {
-                fail("Wrong Test");
-            }
-            code.validate(aca);
-            assertTrue(true);
+            code.init();
+            boolean __tamagotest_oracle = ((code.getOpNumber() < 0) && acaInitialised);
+            assertTrue(__tamagotest_oracle);
         }
         catch(Exception exc) {
             fail(("Test failed at step 5. " + exc.getMessage()));
-        }
-        // Step: 6 Method: init
-        // Transition: completed--{init}-->initialized
-        try {
-            if(!) {
-                fail("Wrong Test");
-            }
-            code.init();
-            boolean __tamagotest_oracle = (code.getOpNumber() < 0);
-            assertTrue(__tamagotest_oracle);
-        }
-        catch(Exception exc) {
-            fail(("Test failed at step 6. " + exc.getMessage()));
-        }
-        // Step: 7 Method: deposit
-        // Transition: initialized--{deposit}-->deposed
-        try {
-            // TODO fulfill the parameter: aca
-            tamago.ext.aca2.ACA aca;
-            if(!) {
-                fail("Wrong Test");
-            }
-            code.deposit(aca);
-            assertTrue(true);
-        }
-        catch(Exception exc) {
-            fail(("Test failed at step 7. " + exc.getMessage()));
-        }
-        // Step: 8 Method: register
-        // Transition: deposed--{register}-->registered
-        try {
-            // TODO fulfill the parameter: aca
-            tamago.ext.aca2.ACA aca;
-            if(!) {
-                fail("Wrong Test");
-            }
-            code.register(aca);
-            boolean __tamagotest_oracle = (code.getOpNumber() > 0);
-            assertTrue(__tamagotest_oracle);
-        }
-        catch(Exception exc) {
-            fail(("Test failed at step 8. " + exc.getMessage()));
-        }
-        // Step: 9 Method: check
-        // Transition: registered--{check}-->waited
-        try {
-            // TODO fulfill the parameter: aca
-            tamago.ext.aca2.ACA aca;
-            if(!) {
-                fail("Wrong Test");
-            }
-            code.check(aca);
-            boolean __tamagotest_oracle = (code.getOpNumber() > 0);
-            assertTrue(__tamagotest_oracle);
-        }
-        catch(Exception exc) {
-            fail(("Test failed at step 9. " + exc.getMessage()));
         }
     }
 }
