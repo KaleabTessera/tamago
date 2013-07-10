17e933ab8cec801a1e36df6dc8fc919a55215e44
aliquandohb@gmail.com <aliquandohb@gmail.com@69474d8d-b9df-4164-16d5-29f8cee7c292>
2013-07-10 15:31:55 +0000

    fix bug with double parsing TODO: manage the filestorage of eclipse in the tamago part for creating new resource directly

--- trunk/TamagoCDLPlugin/src/org/tamago/eclipse/cdl/editor/util/CompileCDLAction.java
+++ trunk/TamagoCDLPlugin/src/org/tamago/eclipse/cdl/editor/util/CompileCDLAction.java
@@ -10,6 +10,7 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IPathEditorInput;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -27,7 +28,7 @@ public class CompileCDLAction extends Action {
 	 */
 	public CompileCDLAction() {
 		super("XML Contract generation");
-		String tooltip = "Génère le Contrat dans le répertoire '../xmls'. Cela suppose donc que votre fichier soit dans le répertoire 'cdls'.";
+		String tooltip = "Genere le Contrat dans le repertoire '../xmls'. Cela suppose donc que votre fichier soit dans le repertoire 'cdls'.";
 		this.setToolTipText(tooltip);
 		this.setDescription(tooltip);
 	}
@@ -41,16 +42,28 @@ public class CompileCDLAction extends Action {
 		System.out.println("Compilation");
 		try   {
 			CDLEditorPlugin.getDefault().clearConsole();
-			CDLEditorPlugin.getDefault().log("TamagoCC for CDL specification - version 0.3");
+			CDLEditorPlugin.getDefault().log("TamagoCC for CDL specification - version 0.4");
 			CDLEditorPlugin.getDefault().log("\tauthor: Hakim Belhaouari");
-			CDLEditorPlugin.getDefault().log("Parser: JavaPop2");
-			CDLEditorPlugin.getDefault().log("\tauthor: Frederic Peschanski");
+			CDLEditorPlugin.getDefault().log("Parser: Antlr");
+			//CDLEditorPlugin.getDefault().log("\tauthor: Frederic Peschanski");
 			
 			IWorkbench workbench = PlatformUI.getWorkbench();
 			IEditorPart editor = workbench.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 			IEditorInput input = (IEditorInput) editor.getEditorInput(); // il semblerait qu'il utilise ca
 			//CDLEditorPlugin.getDefault().log("Input:"+input.getClass().getName());
 
+			if(input instanceof IFileEditorInput) {
+				IFileEditorInput fileinput = ((IFileEditorInput)input);
+				//File file = path.toFile();
+				CompileCDL ccc = new CompileCDL(fileinput);
+				
+				ProgressMonitorDialog pmd = new ProgressMonitorDialog(workbench.getDisplay().getActiveShell());
+				
+				pmd.run(true, false, ccc);
+			}
+			else
+				throw new Exception("Unsupported input: "+input.getClass().getName());
+			/*
 			if(input instanceof IPathEditorInput) {
 				IPath path = ((IPathEditorInput)input).getPath();
 				File file = path.toFile();
@@ -59,7 +72,7 @@ public class CompileCDLAction extends Action {
 				ProgressMonitorDialog pmd = new ProgressMonitorDialog(workbench.getDisplay().getActiveShell());
 				
 				pmd.run(true, false, ccc);
-			}
+			}*/
 		}
 		catch(Exception e) {
 			e.printStackTrace();
